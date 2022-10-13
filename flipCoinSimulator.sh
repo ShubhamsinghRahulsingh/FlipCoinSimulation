11a13be8c6779fa79c1a4612fb5bd39769d27397
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-13 17:33:27 +0530

    Added: Using loop mulpiple times to show number of times head or tail won

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,21 @@
 echo " Welcome to the flip coin simulation program"
 
+read -p "Enter the time upto which you want to flip a coin: " n;
 
-flip=$(($(($RANDOM%10))%2))
-echo "coin flip is $flip"
-if [ $flip -eq 1 ]
-      then
-          echo "Heads"
-          echo "Winner"
-      else
-          echo "Tails"
-fi
+count_1=0
+count_2=0
+
+for (( i=1; i<=n; i++ ))
+do
+       flip=$(($(($RANDOM%10))%2))
+       if [ $flip -eq 1 ]
+       then
+            count_1=$(($count_1 + 1))
+       else
+            count_2=$(($count_2 + 1))
+       fi
+done
+
+echo "Heads won $count_1 times"
+echo "Tails won $count_2 times"
 
