dde5ff0776ebaf6a86354bc79dd3749a998c43ff
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-10-13 18:45:01 +0530

    Refector: If flipping coin ties then continues till difference of 2 is Achieved

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -30,7 +30,31 @@ elif [ $count_2 -eq 21 ]
 then
      diff=$(( $count_2 - $count_1 ))
      echo "Tails Wins by $diff"
-else
-    echo "Its a Tie"
+elif [ $count_1 -eq $count_2 ]
+then
+     echo "Its a Tie"
+     head=0
+     tails=0
+     while true
+     do
+          flip=$((RANDOM%2))
+          if [ $flip -eq 1 ]
+          then
+               heads=$(($heads+1))
+               if [ $(($heads-$tails)) -eq 2 ]
+               then
+                   echo "Heads win"
+               break
+               fi
+          else
+               tails=$(($tails+1))
+               if [ $(($tails-$heads)) -eq 2 ]
+               then
+                   echo "Tails win"
+               break
+               fi
+          fi
+     done
+
 fi
 
